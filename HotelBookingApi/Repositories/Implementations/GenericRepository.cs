d13cbb33c042e9dc1b2ae28172bd8abf2ca29242
shiran abbasi <s.abbasi@tawkeel.com>
2022-06-02 18:27:29 +0500

    generic repo

---
+++ HotelBookingApi/Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using HotelBookingApi.Models;
+using HotelBookingApi.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingApi.Repositories.Implementations
+{
+    public class GenericRepository : IGenericRepository
+    {
+        private readonly ApplicationContext _context;
+        public GenericRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public void Create<T>(T entity) where T : class
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Delete<T>(object id) where T : class
+        {
+            T result = _context.Set<T>().Find(id);
+            if (result != null)
+                _context.Set<T>().Remove(result);
+        }
+
+        public T Get<T>(object id) where T : class
+        {
+            T result = _context.Set<T>().Find(id);
+            return result;
+        }
+
+        public List<T> Get<T>() where T : class
+        {
+            List<T> result = _context.Set<T>().ToList();
+            return result;
+        }
+
+        public List<T> Get<T>(Func<T, bool> filter) where T : class
+        {
+            List<T> result = _context.Set<T>().Where(filter).ToList();
+            return result;
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            _context.Entry<T>(entity).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ HotelBookingApi/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelBookingApi.Repositories.Interfaces
+{
+    public interface IGenericRepository
+    {
+        void Create<T>(T entity) where T : class;
+        T Get<T>(object id) where T : class;
+        List<T> Get<T>() where T : class;
+        List<T> Get<T>(Func<T, bool> filter) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(object id) where T : class;
+        void Save();
+    }
+}
